c2d5e7090125fad79314513294ff51432e8a32b3
Igor C. Batista <igor.batista@gmail.com>
2023-02-08 16:55:10 -0300

    remove comments

--- app/controllers/advances_controller.rb
+++ app/controllers/advances_controller.rb
@@ -95,28 +95,6 @@ class AdvancesController < ApplicationController
   def print
     pdf = AdvancePrint.new(@advance)
    
-    # pdf = Prawn::Document.new(AdvancePrint::PDF_OPTIONS) do |pdf|
-    #   pdf.fill_color "40464e"
-    #   pdf.text "Ruby Metaprogramming", :size => 40, :style => :bold, :align => :center
-
-    #   pdf.move_down 30
-    #   pdf.text "Certificado", :size => 24, :align => :center, :style => :bold
-
-    #   pdf.move_down 30
-    #   pdf.text "Certificamos que <b>Nando Vieira</b> participou...", :inline_format => true
-
-    #   pdf.move_down 15
-    #   pdf.text "São Paulo, #{Time.now.to_formatted_s(:short)}"
-
-    #   pdf.move_down 30
-    #   # pdf.font Rails.root.join("fonts/custom.ttf")
-    #   pdf.text "howto", :size => 24
-
-    #   pdf.move_up 5
-    #   pdf.font "Helvetica"
-    #   pdf.text "http://howtocode.com.br", :size => 10
-    # end
-
     send_data(pdf.render, filename: "Advance_#{@advance.id}.pdf", type: 'application/pdf', disposition: 'inline')
   end
 
